f657864b0caa41f9a4da363d3fbfe663d3838b2d
Hizkia Crisantino <hizkiacrisantino98@gmail.com>
2025-05-08 14:32:50 +0700

    ets-1

--- assets-ets/js/script.js
+++ assets-ets/js/script.js
@@ -5,3 +5,27 @@
  */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.getElementById("memberForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // mencegah submit default
+  const form = e.target;
+
+  // Validasi manual gender (karena radio button butuh perlakuan khusus)
+  const genderChecked = document.querySelector('input[name="gender"]:checked');
+  const genderError = document.getElementById("genderError");
+
+  if (!genderChecked) {
+    genderError.style.display = "block";
+  } else {
+    genderError.style.display = "none";
+  }
+
+  // Validasi form
+  if (form.checkValidity() && genderChecked) {
+    swal(`Pendaftaran berhasil! Selamat menjadi member Pakuwon Mall.`);
+    form.reset();
+    form.classList.remove("was-validated");
+  } else {
+    form.classList.add("was-validated");
+  }
+});
